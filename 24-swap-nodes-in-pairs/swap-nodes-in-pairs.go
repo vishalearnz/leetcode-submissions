eba4ac25299bc568d1c9a8847bca56e79707ae01
Vishal Burman <46467656+vishalearnz@users.noreply.github.com>
2026-08-08 18:57:27 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (28.35%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
+    first := head
+    second := head.Next
+
+    first.Next = swapPairs(second.Next)
+
+    second.Next = first
+
+    return second
+    
+}
